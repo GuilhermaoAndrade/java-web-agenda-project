a29d6a19526833b1459c3485294b3024192004b5
LABORATORIO_INFO <LABORATORIO_INFO@10.67.96.98>
2019-03-14 22:26:50 -0300

    Criação da classe pessoa , e melhoramento do formulario

--- src/com/agenda/servlet/CadastraUsuarioServlet.java
+++ src/com/agenda/servlet/CadastraUsuarioServlet.java
@@ -9,33 +9,31 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.agenda.model.Pessoa;
 
-public class CadastraUsuarioServlet extends HttpServlet
-{
+public class CadastraUsuarioServlet extends HttpServlet {
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		Pessoa pessoa = new Pessoa();
 		pessoa.setNome(request.getParameter("nome"));
 		pessoa.setEndereco(request.getParameter("endereco"));
 		pessoa.setTelefone(request.getParameter("telefone"));
 		pessoa.setEmail(request.getParameter("email"));
-		
-		
-		
-		request.getParameter("nome");
-		System.out.println(request.getParameter("nome"));
-		
-		request.getParameter("endereco");
-		System.out.println(request.getParameter("endereco"));
-		
-		request.getParameter("telefone");
-		System.out.println(request.getParameter("telefone"));
-		
-		request.getParameter("email");
-		System.out.println(request.getParameter("email"));
-		
-		
+
+		/*
+		 * request.getParameter("nome");
+		 * System.out.println(request.getParameter("nome"));
+		 * 
+		 * request.getParameter("endereco");
+		 * System.out.println(request.getParameter("endereco"));
+		 * 
+		 * request.getParameter("telefone");
+		 * System.out.println(request.getParameter("telefone"));
+		 * 
+		 * request.getParameter("email");
+		 * System.out.println(request.getParameter("email"));
+		 */
+
 	}
 
 }
